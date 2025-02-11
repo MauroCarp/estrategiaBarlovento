8ff31e457bcce0549336647cbc3a81f0eb511e78
Mauro <maurogt.dd@gmail.com>
2025-02-11 12:00:40 -0300

    Modificando Carga Real - Falta Insumos

--- vistas/modulos/modales/cargaReal.modal.php
+++ vistas/modulos/modales/cargaReal.modal.php
@@ -5,7 +5,7 @@
     <div class="modal-dialog">
 
         
-        <div class="modal-content" style="width:900px;margin-left:-25%">
+        <div class="modal-content" style="width:1100px;margin-left:-25%">
 
 
           <!--=====================================
@@ -16,7 +16,7 @@
 
             <button type="button" class="close" data-dismiss="modal">&times;</button>
 
-            <h4 class="modal-title" id="">Carga de Ingresos y Egresos</h4>
+            <h4 class="modal-title" id="">Carga Real</h4>
 
           </div>
 
@@ -31,95 +31,348 @@
               <input type="hidden" class="form-control real" id="monthReal" name="month">
               <input type="hidden" class="form-control" name="campaniaReal" value="<?=$data['estrategia']['campania']?>">
 
-              <div class="row" id="insumosReal"></div>
+              <div class="row">
+
+                <div class="col-lg-12">
+
+                  <div class="row">
+
+                    <div class="col-sm-4">
+  
+                      <div class="form-group">
+    
+                        <label>ADP</label>
+    
+                        <input type="number" min="0" step="0.1" class="form-control real" name="adpReal" value="0">
+    
+                      </div>
+                      
+                    </div>
+
+                    <div class="col-sm-4">
+
+                      <div class="form-group">
+
+                        <label>% Cons. MS</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="msReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
 
               <div class="row">
 
-                <div class="col-sm-4">
+                <div class="col-lg-12">
+                  <h4>Animales</h4>
 
-                  <div class="form-group">
+                  <div class="row">
 
-                    <label>Cantidad Ing.</label>
+                    <div class="col-sm-3">
 
-                    <input type="number" min="0" step="0.1" class="form-control real" name="ingresosReal" value="0">
+                      <div class="form-group">
+
+                        <label>Cantidad Ing.</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="ingresosReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-3">
+
+                      <div class="form-group">
+
+                        <label>Kg Ing.</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="kgIngresosReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-3">
+
+                      <div class="form-group">
+
+                        <label>Precio Kg Ing.</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="precioKgIngresosReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-3">
+
+                      <div class="form-group">
+
+                        <label>A Pagar</label>
+
+                        <select class="form-control" name="aPagarIngresoReal" style="font-weight:bold;color:green" >
+                          <option value="A" style="font-weight:bold;color:green">A</option>
+                          <option value="B" style="font-weight:bold;color:blue">B</option>
+                          <option value="C" style="font-weight:bold;color:rgb(227,216,0)">C</option>
+                          <option value="D" style="font-weight:bold;color:red">D</option>
+                        </select>
+
+                      </div>
+
+                    </div>
+                      
+                  </div>
+
+                  <div class="row">
+
+                    <div class="col-sm-3">
+
+                      <div class="form-group">
+
+                        <label>Cantidad Egr.</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="ventasReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-3">
+
+                      <div class="form-group">
+
+                        <label>Kg Egr.</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="kgVentasReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-3">
+
+                      <div class="form-group">
+
+                        <label>Precio Kg Egr.</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="precioKgIngresosReal" value="0">
+
+                      </div>
+
+                      </div>
+
+                      <div class="col-sm-3">
+
+                      <div class="form-group">
+
+                        <label>A Pagar</label>
+
+                        <select class="form-control" name="aPagarIngresoReal" style="font-weight:bold;color:green" >
+                          <option value="A" style="font-weight:bold;color:green">A</option>
+                          <option value="B" style="font-weight:bold;color:blue">B</option>
+                          <option value="C" style="font-weight:bold;color:rgb(227,216,0)">C</option>
+                          <option value="D" style="font-weight:bold;color:red">D</option>
+                        </select>
+
+                      </div>
+
+                    </div>
 
                   </div>
 
                 </div>
 
-                <div class="col-sm-4">
+              </div>
 
-                  <div class="form-group">
+              <div class="row">
 
-                    <label>Kg Ing.</label>
+                <div class="col-lg-12">
+                  <h4>Insumos</h4>
+                  <div class="row" id="insumosReal"></div>
+                </div>
 
-                    <input type="number" min="0" step="0.1" class="form-control real" name="kgIngresosReal" value="0">
+              </div>
 
-                  </div>
+              <div class="row">
+                
+                <div class="col-lg-12">
+
+                  <h4>Dieta</h4>
+
+                  <div class="row" id="dietaReal"></div>
+              
+                  <small id="alertaDietaReal"><b style="color:red">El total de la dieta no puede ser menor a 100%</b></small>
 
                 </div>
 
-                <div class="col-sm-4">
+              </div>
 
-                  <div class="form-group">
+              <div class="row">
 
-                    <label>ADP</label>
-
-                    <input type="number" min="0" step="0.1" class="form-control real" name="adpReal" value="0">
-
-                  </div>
-
-                </div>
+                <div class="col-lg-3" style="padding-left:0px;">
                   
+                  <h4>Estructura Directa</h4>
+
+                  <div class="row">
+
+                    <div class="col-sm-6" style="padding-right:0px">
+
+                      <div class="form-group">
+
+                        <label>Importe</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="estructuraDirectaImporteReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-6" style="padding-left:0px;">
+
+                      <div class="form-group">
+
+                        <label>A Pagar</label>
+
+                        <select class="form-control" name="estructuraDirectaAPagarReal" style="font-weight:bold;color:green" >
+                          <option value="A" style="font-weight:bold;color:green">A</option>
+                          <option value="B" style="font-weight:bold;color:blue">B</option>
+                          <option value="C" style="font-weight:bold;color:rgb(227,216,0)">C</option>
+                          <option value="D" style="font-weight:bold;color:red">D</option>
+                        </select>
+
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+                <div class="col-lg-3" style="padding-left:0px;">
+                  
+                  <h4>Estructura Indirecta</h4>
+
+                  <div class="row">
+
+                    <div class="col-sm-6" style="padding-right:0px">
+
+                      <div class="form-group">
+
+                        <label>Importe</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="estructuraIndirectaImporteReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-6" style="padding-left:0px;">
+
+                      <div class="form-group">
+
+                        <label>A Pagar</label>
+
+                        <select class="form-control" name="estructuraIndirectaAPagarReal" style="font-weight:bold;color:green" >
+                          <option value="A" style="font-weight:bold;color:green">A</option>
+                          <option value="B" style="font-weight:bold;color:blue">B</option>
+                          <option value="C" style="font-weight:bold;color:rgb(227,216,0)">C</option>
+                          <option value="D" style="font-weight:bold;color:red">D</option>
+                        </select>
+
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+                <div class="col-lg-3" style="padding-left:0px;">
+                  
+                  <h4>Gastos Varios</h4>
+
+                  <div class="row">
+
+                    <div class="col-sm-6" style="padding-right:0px">
+
+                      <div class="form-group">
+
+                        <label>Importe</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="gastosVariosImporteReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-6" style="padding-left:0px;">
+
+                      <div class="form-group">
+
+                        <label>A Pagar</label>
+
+                        <select class="form-control" name="gastosVariosPagarReal" style="font-weight:bold;color:green" >
+                          <option value="A" style="font-weight:bold;color:green">A</option>
+                          <option value="B" style="font-weight:bold;color:blue">B</option>
+                          <option value="C" style="font-weight:bold;color:rgb(227,216,0)">C</option>
+                          <option value="D" style="font-weight:bold;color:red">D</option>
+                        </select>
+
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
+                <div class="col-lg-3" style="padding-left:0px;">
+                  
+                  <h4>Ingreso Extraordinario</h4>
+
+                  <div class="row">
+
+                    <div class="col-sm-6" style="padding-right:0px">
+
+                      <div class="form-group">
+
+                        <label>Importe</label>
+
+                        <input type="number" min="0" step="0.1" class="form-control real" name="ingresoExtraImporteReal" value="0">
+
+                      </div>
+
+                    </div>
+
+                    <div class="col-sm-6" style="padding-left:0px;">
+
+                      <div class="form-group">
+
+                        <label>A Pagar</label>
+
+                        <select class="form-control" name="ingresoExtraAPagarReal" style="font-weight:bold;color:green" >
+                          <option value="A" style="font-weight:bold;color:green">A</option>
+                          <option value="B" style="font-weight:bold;color:blue">B</option>
+                          <option value="C" style="font-weight:bold;color:rgb(227,216,0)">C</option>
+                          <option value="D" style="font-weight:bold;color:red">D</option>
+                        </select>
+
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
               </div>
 
-              <div class="row">
-
-                <div class="col-sm-4">
-
-                  <div class="form-group">
-
-                    <label>Cantidad Egr.</label>
-
-                    <input type="number" min="0" step="0.1" class="form-control real" name="ventasReal" value="0">
-
-                  </div>
-
-                </div>
-
-                <div class="col-sm-4">
-
-                  <div class="form-group">
-
-                    <label>Kg Egr.</label>
-
-                    <input type="number" min="0" step="0.1" class="form-control real" name="kgVentasReal" value="0">
-
-                  </div>
-
-                </div>
-
-                <div class="col-sm-4">
-
-                  <div class="form-group">
-
-                    <label>% Cons. MS</label>
-
-                    <input type="number" min="0" step="0.1" class="form-control real" name="msReal" value="0">
-
-                  </div>
-
-                </div>
-
-              </div>
-              
-              <hr>
-
-              <h3>Dieta</h3>
-
-              <div class="row" id="dietaReal"></div>
-              
-              <small id="alertaDietaReal"><b style="color:red">El total de la dieta no puede ser menor a 100%</small>
             </div>
 
           </div>
