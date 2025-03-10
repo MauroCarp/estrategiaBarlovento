85371ebc1713a99044c8c28185711dc8b085e4ee
Mauro <maurogt.dd@gmail.com>
2025-03-10 13:27:10 -0300

    fix porcentajesDietaReal[index]

--- vistas/modulos/estrategia.php
+++ vistas/modulos/estrategia.php
@@ -894,7 +894,7 @@ let calculateStockAndTotals = () => {
         if(isNaN(consMS))
           consMS = Number($(`#msReal${index}`).html())
 
-        let consumoInsumo = (porcentajesDietaReal == null) ? 0 : (porcentajesDietaReal[index].porcentaje * consMS) / 100 
+        let consumoInsumo = ((porcentajesDietaReal == null || porcentajesDietaReal == undefined) || porcentajesDietaReal[index] == null) ? 0 : (porcentajesDietaReal[index].porcentaje * consMS) / 100 
 
         let totalConsumoMensual = consumoInsumo * stockMesPlan * 30 //dias del mes
   
